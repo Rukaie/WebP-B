a7b388084261de3626291d5705af31e03cb44988
Motty <61535898+Rukaie@users.noreply.github.com>
2021-01-13 15:54:04 +0900

    0113-15:53

--- loginform.php
+++ loginform.php
@@ -1,19 +1,46 @@
+<?php
+  session_start();
+
+  $error_message="";
+
+  if(isset($_POST["login"])){
+    if($_POST["userName"]=="web" && $_POST["password"]=="web"){
+      $_SESSION["userName"]=$_POST["userName"];
+      $loginSuccessUrl="login_success.php";
+      header("Location: {$loginSuccessUrl}");
+      exit;
+    }
+    $error_message="ID,もしくはパスワードが間違っています。<br>もう一度入力してください。";
+
+  }?>
+
 <!DOCTYPE html>
 <html lang="ja">
   <head>
     <meta charset="utf-8">
     <title>loginform</title>
+    <link rel="stylesheet" type="text/css" href="loginform_style.css">
   </head>
   <body>
     <header>
     </header>
+    <?php if($error_message){
+      echo $error_message;
+    }?>
+    
     <content>
-        <h1>login form</h1>
-        <form action="login_submit.php" method="get">
-        username<input type="text" name="username"><br>
-        password <input type="password" name="password"><br>
-        <input type="checkbox" name="agree" value="3">I agree to the Privacy Policy.<br>
-        <input type="submit" value="Send">
+        <div class="title"><h1>login form</h1></div>
+        <form action="loginform.php" method="POST">
+
+        <p>username<span>*</span><br>
+        <input type="text" name="userName" class="question" required></p>
+
+        <p>password<span>*</span><br>
+        <input type="password" name="password" class="question" required></p>
+
+        <input type="checkbox" name="agree" value="3" required>I agree to the Privacy Policy.<br>
+        <input type="submit" value="Send" class="button" name="login">
+
     </content>
     <footer>
     </footer>
