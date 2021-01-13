9924df67ab9c2090963dc678ca313852a1859412
Motty <61535898+Rukaie@users.noreply.github.com>
2021-01-13 16:00:50 +0900

    chihiroアップでぃーと

---
+++ common.js
@@ -0,0 +1,12 @@
+function header(rootDir){
+    $.ajax({
+        url: rootDir + "header.html",
+        cache: false, 
+        async: false,
+        success: function(html){
+
+            html = html.replace(/\{\$root\}/g, rootDir);
+            document.write(html);
+        }
+    });
+}
